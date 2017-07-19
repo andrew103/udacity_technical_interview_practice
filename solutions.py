87519ad7953172b63073a1a64f456e434db0e896
Andrew Euredjian <andrew_103@rocketmail.com>
2017-07-19 10:36:32 -0700

    Complete question1

--- solutions.py
+++ solutions.py
@@ -1,5 +1,33 @@
 #============================ QUESTION_1 ======================================
+def question1(s, t):
+    """
+    Takes in two strings, s and t, and compares to see if t is a substring of s
+    """
+    if s and t:
+        # convert strings to lists of letters
+        main = [letter for letter in s.lower()]
+        anagram = [letter for letter in t.lower()]
+        # if all letters popped out of t, the anagram is a substring
+        while len(anagram) > 0:
+            if anagram[0] in main:
+                # for each matching letter, pop that letter out of both strings
+                main.pop(main.index(anagram[0]))
+                anagram.pop(0)
+            else:
+                return False
 
+        return True
+    return False
+
+
+print(question1("udacity", "ad"))
+# Expected result: True
+print(question1("", ""))
+# Expected result: False
+print(question1("mississippi", "misses"))
+# Expected result: False
+print(question1("people", "PEPE"))
+# Expected result: True
 #============================ QUESTION_1 ======================================
 
 
