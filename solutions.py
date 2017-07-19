276019294c7a8c0a0997604f23ad80534c118585
Andrew Euredjian <andrew_103@rocketmail.com>
2017-07-19 14:19:55 -0700

    Complete question2

--- solutions.py
+++ solutions.py
@@ -1,3 +1,4 @@
+print("================= QUESTION 1 =================")
 #============================ QUESTION_1 ======================================
 def question1(s, t):
     """
@@ -29,23 +30,66 @@ print(question1("mississippi", "misses"))
 print(question1("people", "PEPE"))
 # Expected result: True
 #============================ QUESTION_1 ======================================
-
-
+print("")
+print("================= QUESTION 2 =================")
 #============================ QUESTION_2 ======================================
+def longest_palindrome(pal_list):
+    longest = ""
+    for palindrome in pal_list:
+        if len(palindrome) > len(longest):
+            longest = palindrome
 
+    return longest
+
+def find_palindrome(string, lower, upper):
+    distance = 1
+    while string[lower] == string[upper]:
+        upper += 1
+        lower -= 1
+        if upper >= len(string) or lower <= -1:
+            break;
+
+    return string[lower+1:upper]
+
+def question2(a):
+    palindromes = []
+    if a:
+        a = a.replace(" ", "")
+        a = a.lower()
+        for pivot in range(len(a)):
+            try:
+                if a[pivot - 1] == a[pivot + 1]:
+                    palindromes.append(find_palindrome(a, pivot, pivot))
+                elif a[pivot] == a[pivot + 1]:
+                    palindromes.append(find_palindrome(a, pivot, pivot + 1))
+            except IndexError:
+                pass
+
+        return longest_palindrome(palindromes)
+    return None
+
+
+print(question2("asd;flaoeuigvn;alsdknc;MadamiugsadflRepaperjahfvvfeMome"))
+# Expected result: repaper
+print(question2("MadamRotorRedderfMy gymRacecar"))
+# Expected result: racecar
+print(question2(""))
+# Expected result: None
+print(question2("Step on no petsSagasRepaper"))
+# Expected result: steponnopets
 #============================ QUESTION_2 ======================================
-
-
+print("")
+print("================= QUESTION 3 =================")
 #============================ QUESTION_3 ======================================
 
 #============================ QUESTION_3 ======================================
-
-
+print("")
+print("================= QUESTION 4 =================")
 #============================ QUESTION_4 ======================================
 
 #============================ QUESTION_4 ======================================
-
-
+print("")
+print("================= QUESTION 5 =================")
 #============================ QUESTION_5 ======================================
 
 #============================ QUESTION_5 ======================================
